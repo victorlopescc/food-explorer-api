7f557ecfcb6e3e94c3beabcaea755987f95ce4da
victorlopescc <victorlopes.cc@gmail.com>
2024-02-03 17:39:05 -0300

    feat: Knex db

---
+++ src/database/knex/migrations/createUsers.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable('users', table => {
+    table.increments('id');
+
+    table.text('name').notNullable();
+    table.text('email').notNullable();
+    table.text('password').notNullable();
+
+    table.boolean('is_admin').notNullable().defaultTo(false);
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable('users');
